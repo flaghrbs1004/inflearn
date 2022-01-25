a2ecd66f829967faed1f5703bfed4af58adfb120
hogyun lim <office2020lim@gmail.com>
2022-01-25 16:50:36 +0900

    페이징 방법 수정

--- 파이썬-크롤링-기초/3.3페이징가져오기.py
+++ 파이썬-크롤링-기초/3.3페이징가져오기.py
@@ -4,10 +4,12 @@ import pyautogui
 
 # keyword = input("검색어 작성>>")
 keyword = pyautogui.prompt("검색어 작성>>")
-lastPageNum = pyautogui.prompt("몇 페이지까지 궁금하늬?")
+lastPage = pyautogui.prompt("몇 페이지까지 궁금하늬?")
+lastPageNum = int(lastPage) + 1
+
 index: int = 1
 
-for page in range(0, int(lastPageNum), 1):
+for page in range(0, lastPageNum, 1):
     response = requests.get(f"https://search.naver.com/search.naver?where=news&sm=tab_jum&query={keyword}&start={page * 10 + 1}")
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
